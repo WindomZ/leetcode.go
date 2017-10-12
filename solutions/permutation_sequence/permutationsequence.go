fca18b6257b81c22b28d37cc7dfee901393d9ff7
WindomZ <git.windomz@gmail.com>
2017-10-13 00:18:54 +0800

    fix(solutions): permutation_sequence

--- solutions/permutation_sequence/permutationsequence.go
+++ solutions/permutation_sequence/permutationsequence.go
@@ -8,13 +8,13 @@ func getPermutation(n int, k int) string {
 	i, j, f := 1, 0, 1
 	for ; i <= n; i++ {
 		f *= i
-		s[i-1] += byte('0' + i) // make s become 1234...n
+		s[i-1] += byte('0' + i) // make s become 1...n
 	}
 	for i, k = 0, k-1; i < n; i++ {
 		f /= n - i
-		j = i + k/f // calculate and put at s[i]
+		j = i + k/f
+		// move s[j] to s[i] and move s[i...j] to s[i+1...j+1].
 		temp := s[j]
-		// remove c by shifting to cover up (adjust the right part).
 		for ; j > i; j-- {
 			s[j] = s[j-1]
 		}
